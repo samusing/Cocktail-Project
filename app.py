3fdeaeeffe4df1e83ad3039278696885838eebee
samusing <55567829+samusing@users.noreply.github.com>
2020-05-01 12:45:16 -0400

    Create simple html page

--- app.py
+++ app.py
@@ -1,25 +1,54 @@
 #Import Libraries
 from flask import Flask, render_template
-from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-mysql = MySQL(app)
-
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Thecheezer1!'
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_DB'] = 'drinks_database'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor' #Column can be keys and vlaues can be values in that particular column
 
 @app.route("/")
 def index():
-    cur = mysql.connection.cursor()
-    cocktails = cur.execute("SELECT name FROM drinks_database.cocktails")
-    # return render_template('index.html')
-    return cocktails
+    return render_template('index.html')
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#------------- OLD CODE --------------
+# from flask import Flask, render_template
+# from flask_mysqldb import MySQL
+
+# app = Flask(__name__)
+# mysql = MySQL(app)
+
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = 'Thecheezer1!'
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_DB'] = 'drinks_database'
+# app.config['MYSQL_CURSORCLASS'] = 'DictCursor' #Column can be keys and vlaues can be values in that particular column
+
+# @app.route("/")
+# def index():
+#     cur = mysql.connection.cursor()
+#     cocktails = cur.execute("SELECT name FROM drinks_database.cocktails")
+#     # return render_template('index.html')
+#     return cocktails
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
 
 #Import database
