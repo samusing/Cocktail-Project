4a64f286d299bf732386c19ed06cbe5b112deef0
samusing <55567829+samusing@users.noreply.github.com>
2020-05-01 14:30:33 -0400

    Added bootstrap and nav bar with template inheritance

--- app.py
+++ app.py
@@ -7,9 +7,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    return render_template()
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
 if __name__ == "__main__":
     app.run(debug=True)
-
 
 #------------------- OLD CODE --------------------
 # from flask import Flask, render_template
