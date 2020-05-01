b76e5e5d64c590801952a4b91387b67036af30d8
samusing <55567829+samusing@users.noreply.github.com>
2020-05-01 15:23:29 -0400

    created session

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 #Import Libraries
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+from datetime import timedelta
 
 app = Flask(__name__)
+
+#Define a secret key to encrypt and decrypt session data
+app.secret_key = "hello"
+
+#Permanent Sessions
+#Session data is deleted when browser is closed. Session data is stored in a temporary directory on the server and length of session can be defined
+app.permanent_session_lifetime = timdelta(days=5)
 
 @app.route("/")
 def index():
@@ -10,14 +18,30 @@ def index():
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
+        session.permanent = True #Session will last as long as it is defined above 
         user = request.form["nm"] #store data that was typed into name form through the dictionary key nm (for the input box) store in variable called user
-        return redirect(url_for("user", usr=user))
+        session["user"] = user #store user information in a session
+        return redirect(url_for("user"))
     else:
+        #Check if when are on login page, if we are already logged in, redirect to user page otherwise render login page with form
+        if "user" in session:
+            return redirect(url_for("user"))
+        #Render login page
         return render_template('login.html')
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route("/user")
+def user():
+    #Check if there is any information in the session
+    if "user" in session:
+        user = session["user"] #Access user value after you have checked that it exists
+        return f"<h1>{user}</h1>" 
+    else:
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None) #Remove user data from session, remove session data from dictionary
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug=True)
@@ -55,4 +79,7 @@ if __name__ == "__main__":
 #     database="drinks_database"
 # )
 
-
+#Redirect user to page that shows them the name they entered into the form
+# @app.route("/<usr>")
+# def user(usr):
+#     return f"<h1>{usr}</h1>"
